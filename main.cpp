35a3c8b687ea3ee54b4c8ac32ef2ce5a9bb06c9a
schips <schips@dingtalk.com>
2024-07-09 14:20:18 +0800

    Add Cmake and main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using std::ios;
+using std::cout;
+using std::endl;
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+// C code...
+
+#ifdef __cplusplus
+}
+#endif
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
